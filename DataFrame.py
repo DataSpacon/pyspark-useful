92b8d52fb471ec354b91cdc133962ecb6e207b94
Spacon <slawpast@o2.pl>
2021-03-20 17:34:22 +0000

    Update DataFrame.py

--- DataFrame.py
+++ DataFrame.py
@@ -12,3 +12,7 @@ Matter_InoviceSum_Outliers = get_outliers(MatterMain_ToJoin4_7_2, 'Invoice_Sum (
 MatterMain_ToJoin4_7_3= MatterMain_ToJoin4_7_2.filter(~MatterMain_ToJoin4_7_2.MainMatterID.isin(*Matter_InoviceSum_Outliers))
                 #.filter(~MatterMain_ToJoin4_7_2.MainMatterID.isin(*Matter_EstDamagesSum_Outliers))\
                 #.filter(~MatterMain_ToJoin4_7_2.MainMatterID.isin(*Matter_InoviceSum_Outliers))
+    
+#########################################################    
+.filter((pf.col('Invoice_Sum (ThisMonth)')<250000) &\
+#########################################################
