96c838d348d3270a3ef69d0133c163f60ca2adf8
Spacon <slawpast@o2.pl>
2021-03-29 13:21:03 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,9 +1,19 @@
-#UDF functions are applied on rows. They require to be registered as UDFs
-
+# function to create list
 def unpack_list(t):
     return list(set([item for sublist in t fro item in sublist]))
 
 unpack_list_udf = pf.udf(unpack_list, T.ArrayType(T.FloatType()))
 
+# now use that list to  convert 
 sub = sub.withColumn("Prices", unpack_list_udf(pf.col("Prices")))
 
+
+# get the mean based on the abovce functions
+mean_udf = pf.udf(lambda x: float(np.mean(x)),T.FloatType())
+median_udf = pf.udf(lambda x: float(np.median(x)), T.FloatType())
+
+sub = (
+    sub.withColumn("Mean", mean_udf("Prices"))
+    .withColumn("Median", median_udf("Prices"))
+    .drop("Prices")
+)
