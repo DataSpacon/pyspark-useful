4be093cfa290d8020732bf0b8d8d7f2bac573b2a
Spacon <slawpast@o2.pl>
2021-04-02 22:50:42 +0100

    Update DataFrame.py

--- DataFrame.py
+++ DataFrame.py
@@ -58,3 +58,22 @@ for i in numeric_variables:
     newRow = spark.createDataFrame([(i,MatterMain_model_train_lin_reg_1.stat.corr('Invoice_Sum (NextMonth)',i))]\
                                    , columns)
     correlations_0 = correlations_0.union(newRow)
+ 
+ #########################################################################
+ 
+from pyspark.sql.functions import col, isnan, when, trim
+
+df = spark.createDataFrame([
+    ("", 1, 2.0), ("foo", None, 3.0), ("bar", 1, float("NaN")), 
+    ("good", 42, 42.0)])
+
+def to_null(c):
+    return when(~(col(c).isNull() | isnan(col(c)) | (trim(col(c)) == "")), col(c))
+
+
+df.select([to_null(c).alias(c) for c in df.columns]).na.drop().show()
+ 
+ #############################################################################################################
+ highest_correlation_columns = [row.Variable for row in\
+                               Correlations_1.orderBy('Correlation', ascending=False).select('Variable').take(10)]
+ 
